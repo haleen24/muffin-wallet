8bb64a14ae586630a90654ce486ecb360b70666d
Alexey Danilov <aadmain@yandex.ru>
2025-11-16 01:45:44 +0300

    wip

--- muffin-wallet-data/src/main/resources/db/changelog/1.1.0/muffin_transaction.sql
+++ muffin-wallet-data/src/main/resources/db/changelog/1.1.0/muffin_transaction.sql
@@ -2,4 +2,4 @@
 
 --changeset Koshkin George:1d682896-3de3-48a9-9fb4-b568939abeaf
 alter table muffin_transaction add column currency decimal;
---rollback  --do-nothing
+--rollback alter table muffin_transaction drop column currency
--- muffin-wallet-data/src/main/resources/db/changelog/1.1.0/muffin_wallet.sql
+++ muffin-wallet-data/src/main/resources/db/changelog/1.1.0/muffin_wallet.sql
@@ -2,4 +2,4 @@
 
 -- changeset Koshkin George:14af988c-0326-4d02-8b16-57ca8c1deae7
 alter table muffin_wallet add column type varchar;
---rollback --do-nothing
+--rollback alter table muffin_wallet drop column type
